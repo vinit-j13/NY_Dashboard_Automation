b7bb84559e6f7395466cbd74c99241717dab044e
ajay <aj05kumar1994@gmail.com>
2024-12-19 18:43:41 +0530

    RideSeach_Ridecancellation_XpathChange_MethodCalling

--- src/test/java/bapFlows/BapRideSearch.java
+++ src/test/java/bapFlows/BapRideSearch.java
@@ -16,43 +16,31 @@ public class BapRideSearch extends BaseClass {
 		String userPhone = "9191919191";
 		System.out.println("Starting rideSearch....");
 		
-		//Taxi Booth
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/a/div/div[2]")).click();
-		//Enter mobile number
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div[2]/div/form/div[1]/div/div[2]/div[2]/input")).sendKeys(userPhone);
-		Thread.sleep(2000);
-		//Send otp Button
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div[2]/div/form/div[2]/div/button[2]")).click();
-		Thread.sleep(2500);
-		//Enter otp
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div[2]/div/form/div[1]/div/div/div[2]/input")).sendKeys("7891");
-		Thread.sleep(2000);
-		//submit otp
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div[2]/div/form/div[2]/button[2]")).click();
-		Thread.sleep(2000);
-		//search destination for user button 
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div/div[4]/button")).click();
-		Thread.sleep(2000);
-		//Location dropdown
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/form/div[1]/div[1]/div[1]/div/div[2]/div/div/div/button")).click();
-		Thread.sleep(2000);		
-		//santragachi railway station select from dropdown
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/form/div[1]/div[1]/div[1]/div/div[2]/div/div[2]/div/div/div[3]/div/div[1]/div/div/div")).click();
-		Thread.sleep(2000);
-		//Drop location(Search destination)
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/form/div[1]/div[2]/div[2]/div/input")).sendKeys("kolkata");
-		Thread.sleep(3500);
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/form/div[1]/div[2]/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/div/div")).click();
-		Thread.sleep(3000);
-		//Get Fares button
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/form/div[2]/button[2]")).click();
-		Thread.sleep(5000);
-		//Select estimates
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div[3]/div[1]")).click();
-		Thread.sleep(2000);
-		//Confirm the booking popup
-		driver.findElement(By.xpath("/html/body/div/div/div/div/div/div[2]/div/div[3]/div/button[2]")).click();
+        BapRideCancellation ridebook = new BapRideCancellation();
+        
+        ridebook.goToTaxiBooth();
+        
+        ridebook.enterPhoneNumber(userPhone);
+        
+        ridebook.sendOtp();
+
+        ridebook.enterOtp("7891");
+        
+        ridebook.submitOtp();
+
+        ridebook.searchDestinationForUser();
+
+        ridebook.selectLocation();
+        
+        ridebook.enterDropLocation("kolkata");
+        
+        ridebook.getFares();
+
+        ridebook.selectEstimate();
+
+        ridebook.confirmBooking();
+
+		
 		Thread.sleep(4500);
 		BapRideSearch.OTP = copyOtp();
 		System.out.println("Ended rideSearch!!");
@@ -61,7 +49,7 @@ public class BapRideSearch extends BaseClass {
 	@Test
 	//Copy ride otp function
 	public String copyOtp() throws InterruptedException {
-	WebElement rideOtpElement = driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/div/div[3]/div/div[1]/div[1]/div/div[2]/div[2]"));
+	WebElement rideOtpElement = driver.findElement(By.xpath("//div[@class='font-bold text-fs-20 not-italic dark:text-white']"));
 	Thread.sleep(2000);
 	String rideOtp = rideOtpElement.getAttribute("textContent");
 	return rideOtp;
