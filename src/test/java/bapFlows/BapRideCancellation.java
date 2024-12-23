185b3ddfb181e0de70b51f8a512842bef46541b8
Vinit Joshi <vinitjoshi@Vinits-MacBook-Pro.local>
2024-12-23 19:12:30 +0530

    Handled user ridesearch if any new user number is entered

--- src/test/java/bapFlows/BapRideCancellation.java
+++ src/test/java/bapFlows/BapRideCancellation.java
@@ -10,24 +10,13 @@ public class BapRideCancellation extends BaseClass {
 	public void userRideCancellation() throws InterruptedException {
 
 		BapHelper bapHelper = new BapHelper();
+		
+		BapRideSearch rideSearch = new BapRideSearch();
 
-		String userPhone = "9191919191";
 		System.out.println("Starting user ride cancellation....");
 
-		// Perform the steps up to the "Confirm the booking popup"
-		bapHelper.goToTaxiBooth();
-		bapHelper.enterPhoneNumber(userPhone);
-		bapHelper.sendOtp();
-		bapHelper.enterOtp("7891");
-		bapHelper.submitOtp();
-		bapHelper.searchDestinationForUser();
-		bapHelper.selectLocation();
-		bapHelper.enterDropLocation("kolkata");
-		bapHelper.getFares();
-		bapHelper.selectEstimate();
-
-		// Confirm the booking popup
-		bapHelper.confirmBooking();
+		//BapRideSearch flow is same till here
+		rideSearch.rideSearchFlow();
 		Thread.sleep(2000);
 
 		// Proceed with cancellation
--- src/test/java/bapFlows/BapRideSearch.java
+++ src/test/java/bapFlows/BapRideSearch.java
@@ -1,13 +1,12 @@
 package bapFlows;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
 import baseClass.BaseClass;
 import utils.BapHelper;
 
@@ -17,6 +16,7 @@ public class BapRideSearch extends BaseClass {
 
 	@Test
 	public void rideSearchFlow() throws InterruptedException {
+
 		BapHelper bapHelper = new BapHelper();
 
 		System.out.println("Starting rideSearch....");
@@ -30,21 +30,74 @@ public class BapRideSearch extends BaseClass {
 		bapHelper.enterOtp(userOtp);
 
 		bapHelper.submitOtp();
-
-		bapHelper.searchDestinationForUser();
-
-		bapHelper.selectLocation();
-
-		bapHelper.enterDropLocation("kolkata");
-
-		bapHelper.getFares();
-
-		bapHelper.selectEstimate();
-
-		bapHelper.confirmBooking();
 		
+		//Handling new or existing user logic starts here
+		try {
+
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+			boolean isNewUser = true;
+
+			try {
+				
+				wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+			}
+
+			catch (TimeoutException e) {
+				System.out.println("Not a new user");
+				isNewUser = false;
+			}
+
+			if (isNewUser) { //if it is a new user this block will run
+
+				System.out.println("entering if else");
+
+				driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+				Thread.sleep(2000);
+
+				wait.until(ExpectedConditions
+						.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+				driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+				Thread.sleep(2000);
+
+				bapHelper.selectLocation();
+
+				bapHelper.enterDropLocation("kolkata");
+
+				bapHelper.getFares();
+
+				bapHelper.selectEstimate();
+
+				bapHelper.confirmBooking();
+			}
+
+			else { //if it is an existing user this block will run
+
+				bapHelper.searchDestinationForUser();
+
+				bapHelper.selectLocation();
+
+				bapHelper.enterDropLocation("kolkata");
+
+				bapHelper.getFares();
+
+				bapHelper.selectEstimate();
+
+				bapHelper.confirmBooking();
+
+			}
+
+		}
+
+		catch (Exception e) {
+			System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+		}
+
 		BapRideSearch.OTP = copyOtp();
-		
+
 		System.out.println("Ended rideSearch!!");
 
 	}
@@ -52,15 +105,18 @@ public class BapRideSearch extends BaseClass {
 	@Test
 	// Copy ride otp function
 	public String copyOtp() throws InterruptedException {
-		
-		WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(20));
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='font-bold text-fs-20 not-italic dark:text-white']")));
-		
-		WebElement rideOtpElement = driver.findElement(By.xpath("//div[@class='font-bold text-fs-20 not-italic dark:text-white']"));
-		
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//div[@class='font-bold text-fs-20 not-italic dark:text-white']")));
+
+		WebElement rideOtpElement = driver
+				.findElement(By.xpath("//div[@class='font-bold text-fs-20 not-italic dark:text-white']"));
+
 		String rideOtp = rideOtpElement.getAttribute("textContent");
-		
+
 		return rideOtp;
 
 	}
+
 }
