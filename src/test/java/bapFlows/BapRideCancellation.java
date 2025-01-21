cbd40f22df6fd8e606411b65f6671e9cdbba950a
Vinit Joshi <vinitjoshi@Vinits-MacBook-Pro.local>
2025-01-21 18:48:18 +0530

    Added flow - User cancellation for all pickup locations in kolkata

--- src/test/java/bapFlows/BapRideCancellation.java
+++ src/test/java/bapFlows/BapRideCancellation.java
@@ -1,7 +1,14 @@
 package bapFlows;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import baseClass.BaseClass;
 import utils.BapHelper;
@@ -9,23 +16,600 @@ import utils.BapHelper;
 public class BapRideCancellation extends BaseClass {
 
 	@Test
-	public void userRideCancellation() throws InterruptedException, IOException {
+	public void userRideCancellationSantargachi() throws InterruptedException, IOException {
+		
+        FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
+		
+		Properties properties = new Properties();
+		
+		properties.load(fr);
 
 		BapHelper bapHelper = new BapHelper();
+
+		System.out.println("Starting rideSearch....");
+
+		bapHelper.goToTaxiBooth();
+
+		bapHelper.enterPhoneNumber(properties.getProperty("userPhone"));
+
+		bapHelper.sendOtp();
+
+		bapHelper.enterOtp(properties.getProperty("userOtp"));
+
+		bapHelper.submitOtp();
 		
-		BapRideSearch rideSearch = new BapRideSearch();
+		//Handling new or existing user logic starts here
+		try {
 
-		System.out.println("Starting user ride cancellation....");
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-		//BapRideSearch flow is same till here
-		rideSearch.rideSearchFlow();
-		Thread.sleep(2000);
+			boolean isNewUser = true;
 
-		// Proceed with cancellation
-		bapHelper.cancelBooking();
-		bapHelper.cancelBookingPopup();
+			try {
+				
+				wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+			}
 
-		System.out.println("Ride cancellation successful.!");
+			catch (TimeoutException e) {
+				System.out.println("Not a new user");
+				isNewUser = false;
+			}
+
+			if (isNewUser) { //if it is a new user this block will run
+
+				System.out.println("entering if else");
+
+				driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+				Thread.sleep(2000);
+
+				wait.until(ExpectedConditions
+						.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+				driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+				Thread.sleep(2000);
+
+				bapHelper.selectLocationSantargachi();
+
+				bapHelper.enterDropLocation("kolkata");
+
+				bapHelper.getFares();
+
+				bapHelper.selectEstimate();
+
+				bapHelper.confirmBooking();
+				
+				bapHelper.cancelBooking();
+				
+				bapHelper.cancelBookingPopup();
+
+			}
+
+			else { //if it is an existing user this block will run
+
+				bapHelper.searchDestinationForUser();
+
+				bapHelper.selectLocationSantargachi();
+
+				bapHelper.enterDropLocation("kolkata");
+
+				bapHelper.getFares();
+
+				bapHelper.selectEstimate();
+
+				bapHelper.confirmBooking();
+				
+				bapHelper.cancelBooking();
+				
+				bapHelper.cancelBookingPopup();
+
+			}
+
+		}
+
+		catch (Exception e) {
+			System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+		}
+		
+	}
+	
+	@Test
+	public void userRideCancellationHowrahStation() throws InterruptedException, IOException{
+		 FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
+			
+			Properties properties = new Properties();
+			
+			properties.load(fr);
+
+			BapHelper bapHelper = new BapHelper();
+
+			System.out.println("Starting rideSearch....");
+
+			bapHelper.goToTaxiBooth();
+
+			bapHelper.enterPhoneNumber(properties.getProperty("userPhone"));
+
+			bapHelper.sendOtp();
+
+			bapHelper.enterOtp(properties.getProperty("userOtp"));
+
+			bapHelper.submitOtp();
+			
+			//Handling new or existing user logic starts here
+			try {
+
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+				boolean isNewUser = true;
+
+				try {
+					
+					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+				}
+
+				catch (TimeoutException e) {
+					System.out.println("Not a new user");
+					isNewUser = false;
+				}
+
+				if (isNewUser) { //if it is a new user this block will run
+
+					System.out.println("entering if else");
+
+					driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+					Thread.sleep(2000);
+
+					wait.until(ExpectedConditions
+							.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+					driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+					Thread.sleep(2000);
+
+					bapHelper.selectLocationHowrahStation();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+				else { //if it is an existing user this block will run
+
+					bapHelper.searchDestinationForUser();
+
+					bapHelper.selectLocationHowrahStation();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+			}
+
+			catch (Exception e) {
+				System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+			}
+
+		
+		
+	}
+	
+	@Test
+	public void userRideCancellationSealdahStation() throws InterruptedException, IOException{
+		 FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
+			
+			Properties properties = new Properties();
+			
+			properties.load(fr);
+
+			BapHelper bapHelper = new BapHelper();
+
+			System.out.println("Starting rideSearch....");
+
+			bapHelper.goToTaxiBooth();
+
+			bapHelper.enterPhoneNumber(properties.getProperty("userPhone"));
+
+			bapHelper.sendOtp();
+
+			bapHelper.enterOtp(properties.getProperty("userOtp"));
+
+			bapHelper.submitOtp();
+			
+			//Handling new or existing user logic starts here
+			try {
+
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+				boolean isNewUser = true;
+
+				try {
+					
+					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+				}
+
+				catch (TimeoutException e) {
+					System.out.println("Not a new user");
+					isNewUser = false;
+				}
+
+				if (isNewUser) { //if it is a new user this block will run
+
+					System.out.println("entering if else");
+
+					driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+					Thread.sleep(2000);
+
+					wait.until(ExpectedConditions
+							.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+					driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+					Thread.sleep(2000);
+
+					bapHelper.selectLocationSealdahStation();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+				else { //if it is an existing user this block will run
+
+					bapHelper.searchDestinationForUser();
+
+					bapHelper.selectLocationSealdahStation();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+			}
+
+			catch (Exception e) {
+				System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+			}
+
+		
+
+		
+	}
+	
+	@Test		
+	public void userRideCancellationKolkataStation() throws InterruptedException, IOException{
+		 FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
+			
+			Properties properties = new Properties();
+			
+			properties.load(fr);
+
+			BapHelper bapHelper = new BapHelper();
+
+			System.out.println("Starting rideSearch....");
+
+			bapHelper.goToTaxiBooth();
+
+			bapHelper.enterPhoneNumber(properties.getProperty("userPhone"));
+
+			bapHelper.sendOtp();
+
+			bapHelper.enterOtp(properties.getProperty("userOtp"));
+
+			bapHelper.submitOtp();
+			
+			//Handling new or existing user logic starts here
+			try {
+
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+				boolean isNewUser = true;
+
+				try {
+					
+					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+				}
+
+				catch (TimeoutException e) {
+					System.out.println("Not a new user");
+					isNewUser = false;
+				}
+
+				if (isNewUser) { //if it is a new user this block will run
+
+					System.out.println("entering if else");
+
+					driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+					Thread.sleep(2000);
+
+					wait.until(ExpectedConditions
+							.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+					driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+					Thread.sleep(2000);
+
+					bapHelper.selectLocationKolkataStation();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+				else { //if it is an existing user this block will run
+
+					bapHelper.searchDestinationForUser();
+
+					bapHelper.selectLocationKolkataStation();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+			}
+
+			catch (Exception e) {
+				System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+			}		
 	}
 
+	@Test
+	public void userRideCancellationKolkataAirport() throws InterruptedException, IOException{
+		 FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
+			
+			Properties properties = new Properties();
+			
+			properties.load(fr);
+
+			BapHelper bapHelper = new BapHelper();
+
+			System.out.println("Starting rideSearch....");
+
+			bapHelper.goToTaxiBooth();
+
+			bapHelper.enterPhoneNumber(properties.getProperty("userPhone"));
+
+			bapHelper.sendOtp();
+
+			bapHelper.enterOtp(properties.getProperty("userOtp"));
+
+			bapHelper.submitOtp();
+			
+			//Handling new or existing user logic starts here
+			try {
+
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+				boolean isNewUser = true;
+
+				try {
+					
+					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+				}
+
+				catch (TimeoutException e) {
+					System.out.println("Not a new user");
+					isNewUser = false;
+				}
+
+				if (isNewUser) { //if it is a new user this block will run
+
+					System.out.println("entering if else");
+
+					driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+					Thread.sleep(2000);
+
+					wait.until(ExpectedConditions
+							.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+					driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+					Thread.sleep(2000);
+
+					bapHelper.selectLocationKolkataAirport();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+				else { //if it is an existing user this block will run
+
+					bapHelper.searchDestinationForUser();
+
+					bapHelper.selectLocationKolkataAirport();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+			}
+
+			catch (Exception e) {
+				System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+			}
+
+
+		
+	}
+
+	@Test
+	public void userRideCancellationGangasagarMela() throws InterruptedException, IOException{
+		 FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
+			
+			Properties properties = new Properties();
+			
+			properties.load(fr);
+
+			BapHelper bapHelper = new BapHelper();
+
+			System.out.println("Starting rideSearch....");
+
+			bapHelper.goToTaxiBooth();
+
+			bapHelper.enterPhoneNumber(properties.getProperty("userPhone"));
+
+			bapHelper.sendOtp();
+
+			bapHelper.enterOtp(properties.getProperty("userOtp"));
+
+			bapHelper.submitOtp();
+			
+			//Handling new or existing user logic starts here
+			try {
+
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+				boolean isNewUser = true;
+
+				try {
+					
+					wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstName']")));
+				}
+
+				catch (TimeoutException e) {
+					System.out.println("Not a new user");
+					isNewUser = false;
+				}
+
+				if (isNewUser) { //if it is a new user this block will run
+
+					System.out.println("entering if else");
+
+					driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("New test user");
+
+					Thread.sleep(2000);
+
+					wait.until(ExpectedConditions
+							.presenceOfElementLocated(By.xpath("//button[@aria-label='updateUserDetails']")));
+
+					driver.findElement(By.xpath("//button[@aria-label='updateUserDetails']")).click();
+
+					Thread.sleep(2000);
+
+					bapHelper.selectLocationGangasagarMela();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+				else { //if it is an existing user this block will run
+
+					bapHelper.searchDestinationForUser();
+
+					bapHelper.selectLocationGangasagarMela();
+
+					bapHelper.enterDropLocation("kolkata");
+
+					bapHelper.getFares();
+
+					bapHelper.selectEstimate();
+
+					bapHelper.confirmBooking();
+					
+					bapHelper.cancelBooking();
+					
+					bapHelper.cancelBookingPopup();
+
+				}
+
+			}
+
+			catch (Exception e) {
+				System.out.println("Some error accoured. PLease check you xpath or timeouts!");
+			}
+
+
+		
+	}
+	
 }
