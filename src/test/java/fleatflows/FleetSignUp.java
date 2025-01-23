5cc1b8df0ff43d83bbdc8948ea322cea52700c0e
Sai Krishna Todupunuri <sai.krishna@nammayatri.in>
2025-01-23 19:12:00 +0530

    fleet changes added flows

--- src/test/java/fleatflows/FleetSignUp.java
+++ src/test/java/fleatflows/FleetSignUp.java
@@ -1,55 +1,20 @@
 package fleatflows;
-
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Properties;
-
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
 import baseClass.BaseClass;
-
+import utils.FleetHelper;
 public class FleetSignUp extends BaseClass {
-
     @Test
-    public void FleetSighnUpFlow() throws InterruptedException, IOException {
-    	FileReader fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/config file/config.properties");
-		Properties properties = new Properties();
-		properties.load(fr);
-        String mobileNumber = "8888888881";
-        String cityName = "NAMMA YATRI PARTNER:Bangalore";
-        driver.get(properties.getProperty("urlfleet"));
-        driver.manage().window().maximize();
-
-        System.out.println("Starting Fleat login flow...");
-
-        
-//        Thread.sleep(2000);
-        if (!driver.getCurrentUrl().contains("dashboard")) {
-            System.out.println("Failed to load the login page. Check the URL!");
-            return;
+    public void FleetLoginFlow() throws IOException {     
+    	try {
+    		FleetHelper fleetHelper = new FleetHelper();
+    		System.out.println("Started....launching Fleet in browser");	
+    		System.out.println("Running tests......");
+    		fleetHelper.fleetSignUp();
+    		System.out.println("Starting Fleet Sighnup...");
+        } catch (Exception e) {
+            System.out.println("An error occurred during Sighnp: " + e.getMessage());
+            e.printStackTrace();
         }
-        
-        driver.findElement(By.xpath("//*[@id=\"email_\"]")).click();  
-        Thread.sleep(2000);
-        
-        //Name
-        driver.findElement(By.xpath("//input[@name ='name']")).sendKeys("testing");
-        
-        //MobileNumber
-        driver.findElement(By.xpath("//div[@class=\"flex flex-row gap-40\"]/descendant::div[26]/input")).sendKeys(mobileNumber);
-        
-        //Email
-        driver.findElement(By.xpath("//input[@name ='email']")).sendKeys("testin@gmail.com");
-        
-        //select City
-        driver.findElement(By.xpath("//div[@class=\"flex flex-row gap-40\"]/descendant::button[01]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//div[@data-text='" + cityName + "']")).click();       
-  
-        //sign-up button
-        driver.findElement(By.xpath("//button[@data-button-for=\"signUp\"]")).click();
-        Thread.sleep(2000);
-
-}
+    }
 }
